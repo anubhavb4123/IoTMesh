2d6b5cd73e47d73fc450e26d025e167e220939bc
Anubhabv Bajpai <anubhavb4123@gmail.com>
2026-04-26 17:33:17 +0530

    feat: add skeleton components for dashboard, sensors, alerts, users, and devices

---
+++ src/components/skeletons/index.ts
@@ -0,0 +1,5 @@
+export { DashboardSkeleton } from "./DashboardSkeleton";
+export { SensorsSkeleton } from "./SensorsSkeleton";
+export { AlertsSkeleton } from "./AlertsSkeleton";
+export { UsersSkeleton } from "./UsersSkeleton";
+export { DevicesSkeleton } from "./DevicesSkeleton";
